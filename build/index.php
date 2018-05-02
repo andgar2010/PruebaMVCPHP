79760780848027a50f013e16b9e9097414312ed9
unknown <afgarcia0479@misena.edu.co>
2018-05-01 22:23:00 -0500

    - Fixed button delete id and alert - Change lines ifelse to switch - Add style css propeller

--- build/index.php
+++ build/index.php
@@ -12,6 +12,8 @@ include('conexion.php');
 
     <!-- Bootstrap 3 -->
     <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../node_modules/propellerkit/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../node_modules/propellerkit/dist/css/propeller.min.css">
 
     <style>
         .content {
@@ -31,45 +33,37 @@ include('conexion.php');
             <?php
             if (isset($_GET['aksi']) == 'delete' ) {
                 // escaping, additionally removing everything that cloud be (html/javascript-) code
-                $nik = mysqli_real_escape_string($con, (strip_tags ($_GET['nik'], ENT_QUOTES) ) );
+                $nik = mysqli_real_escape_string($con, (strip_tags($_GET['nik'], ENT_QUOTES)));
                 $sql_cek = "SELECT * FROM empleados WHERE codigo = '$nik' ";
                 $cek = mysqli_query($con, $sql_cek);
                 if (mysqli_num_rows($cek) == 0) {
-                    echo '<div class="alert info-alert alert-dismissable"> 
-                                <button type="button class="close" data-dismiss="alert" aria-hidden="true">
+                    echo '<div class="alert alert-info alert-dismissable">
+                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
                                     &times;
                                 </button>
                                 No se encontraron datos.
                             </div>';
-                }
-                else {
+                } else {
                     $sql_delete = "DELETE FROM empleados WHERE codigo='$nik'";
                     $delete = mysqli_query($con, $sql_delete);
                     if ($delete) {
-                        echo '<div class="alert alert-success alert alert*dismissable">
-                                    <button type="button" class="close" data-dimiss="alert" aria-hidden="true">
-                                        &times;
-                                    </button>
+                        echo '<div class="alert alert-success alert-dismissable">
+                                    <button type="button" class="close" data-dimiss="alert" aria-hidden="true">&times;</button>
                                     Datos eliminado correctamente.
                                 </div>';
                     } else {
-                        echo 'div class="alert alert-danger alert-dismissable>
-                                    <button type="button" class="close" data-dimiss"alert" aria-hidden="true">
-                                        &times;
-                                    </button>
+                        echo '<div class="alert alert-danger alert-dismissable>
+                                    <button type="button" class="close" data-dimiss"alert" aria-hidden="true">&times;</button>
                                     Error, no se pudo eliminar los datos.
-                                </div>
-                                ';
+                                </div>';
                     }
-
                 }
             }
             ?>
-            <!-- END PHP serach mysql -->
             <!-- FORM  -->
             <form class="form-inline" action="" method="get">
                 <div class="form-group">
-                    <select name="filter" class="form-control" onchange="form.submit()" id="">
+                    <select name="filter" class="form-control" onchange="form.submit()">
                         <option value="0">Filtros de datos de empleados</option>
                         <?php $filter = (isset($_GET['filter']) ? strtolower($_GET['filter']) : NULL ); ?>
                         <option value="1" <?php if($filter == 'Tetap') {echo 'selected';}?>>Fijo</option>
@@ -77,8 +71,7 @@ include('conexion.php');
                         <option value="3" <?php if($filter == 'Outsourcing') {echo 'selected';}?>>Outsourcing</option>
                     </select>
                 </div>
-            </form>
-            <!-- END FORM -->
+            </form><!-- END FORM -->
             <br>
             <div class="table-responsive">
                 <table class="table table-striped table-hover">
@@ -88,55 +81,67 @@ include('conexion.php');
                         <th>Nombre</th>
                         <th>Lugar de nacimiento</th>
                         <th>Fecha de nacimiento</th>
-                        <th>telefono</th>
+                        <th>Teléfono</th>
                         <th>Cargo</th>
                         <th>Estado</th>
                         <th>Acciones</th>
                     </tr>
                     <?php
-                    if ($filter) {
-                        $sql_filter = "SELECT * FROM empleados WHERE estado = '$filter' ORDER BY codigo ASC";
-                        $sql = mysqli_query($con, $sql_filter);
-                    } else {
+                    if ($filter == 0) {
                         $sql_filter = "SELECT * FROM empleados ORDER BY codigo ASC";
-                        $sql = mysqli_query($con, $sql_filter);
+                        $output_sql = mysqli_query($con, $sql_filter);
+                    } else {
+                        $sql_filter = "SELECT * FROM empleados WHERE estado = '$filter' ORDER BY codigo ASC";
+                        $output_sql = mysqli_query($con, $sql_filter);
                     }
-                    if (mysqli_num_rows($sql) == 0) {
-                        echo'<tr>
+
+                    if (mysqli_num_rows($output_sql) == 0) {
+                        echo'
+                            <tr>
                                 <td colspan="8">No hay datos</td>
                             </tr>';
                     } else {
                         $no = 1;
-                        while ($row = mysqli_fetch_assoc($sql)) {
+                        while ($row = mysqli_fetch_assoc($output_sql)) {
                             echo'
-                            <tr>
-                                <td>'.$no.'</td>
-                                <td>'.$row['codigo'].'</td>
-                                <td> 
-                                    <a href="profile.php?nik='.$row['codigo'].'">
-                                        <span class="glyphicon glyphicon-user" aria-hidden="true"></span>'.
-                                        $row['nombres'].
-                                    '</a> </td>
-                                <td>'.$row['lugar_nacimiento'].'</td>
-                                <td>'. $row['fecha_nacimiento'].'</td>
-                                <td>'. $row['telefono'].'</td>
-                                <td>'. $row['puesto'].'</td>';
+                                <tr>
+                                    <td>'.$no.'</td>
+                                    <td>'.$row['codigo'].'</td>
+                                    <td>
+                                        <a href="profile.php?nik='.$row['codigo'].'">
+                                            <span class="glyphicon glyphicon-user" aria-hidden="true">&nbsp;</span>'.
+                                            $row['nombres'].
+                                        '</a> </td>
+                                    <td>'.$row['lugar_nacimiento'].'</td>
+                                    <td>'. $row['fecha_nacimiento'].'</td>
+                                    <td>'. $row['telefono'].'</td>
+                                    <td>'. $row['puesto'].'</td>';
 
-                            if ($row['estado'] == 1) {
-                                echo '
-                                <td>
-                                    <span class="label label-success">Fijo</span>
-                                </td>';
-                            } else if ($row['estado'] == 2) {
-                                echo '
-                                <td>
-                                    <span class="label label-info">Contratado</span>
-                                </td>';
-                            } else if ($row['estado'] == 3) {
-                                echo '
-                                <td>
-                                    <span class="label label-warning">Outsourcing</span>
-                                </td>';
+                            switch ($row['estado']) {
+                                case 1:
+                                    echo '
+                                        <td>
+                                            <span class="label label-success">Fijo</span>
+                                        </td>';
+                                    break;
+                                case 2:
+                                    echo '
+                                        <td>
+                                            <span class="label label-info">Contratado</span>
+                                        </td>';
+                                    break;
+                                case 3:
+                                    echo '
+                                        <td>
+                                            <span class="label label-warning">Outsourcing</span>
+                                        </td>';
+                                    break;
+                                default:
+                                    echo '
+                                        <td>
+                                            <span class="label label-warning">No seleccionado</span>
+                                        </td>';
+                                    break;
                             }
 
                             echo'
@@ -144,7 +149,7 @@ include('conexion.php');
                                 <td>
                                     <a href="edit.php?nik='.$row['codigo'].'" title="Editar datos" class="btn btn-primary btn-sm">
                                         <span class="glyphicon glyphicon-edit" aria-hidden="true"></span> </a>
-                                    <a href="index.php?aksi?=delete&nik='.$row['codigo'].'" title="Eliminar" onclick="return confirm(\'Esta seguro de borrar los datos '.$row['nombres'].'? \')" class="btn btn-danger btn-sm">
+                                    <a href="index.php?aksi=delete&nik='.$row['codigo'].'" title="Eliminar" onclick="return confirm(\'Esta seguro de borrar los datos '.$row['nombres'].'? \')" class="btn btn-danger btn-sm">
                                         <span class="glyphicon glyphicon-trash" aria-hidden="true"></span> </a>
                                     </td>
                             </tr>';
@@ -164,5 +169,7 @@ include('conexion.php');
     </footer>
     <script src="../node_modules/jquery/dist/jquery.min.js"></script>
     <script src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="../node_modules/propellerkit/dist/js/bootstrap.min.js"></script>
+    <script src="../node_modules/propellerkit/dist/js/propeller.min.js"></script>
 </body>
 </html>
