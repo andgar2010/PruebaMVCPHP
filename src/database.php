52366a54c95f17457dc974ad6fa00bac7eddb7c1
unknown <afgarcia0479@misena.edu.co>
2018-05-07 00:20:54 -0500

    Add CRUD PHP Completed

--- src/database.php
+++ src/database.php
@@ -19,26 +19,102 @@
 class Database
 {
     private $_con;
-    private $_dbhost = 'locahost';
+    private $_dbhost = 'localhost';
     private $_dbuser = 'root';
     private $_dbpass = '';
-    private $_dbname = 'tuto_poo';
+    private $_dbname = 'test_poo';
     private $_msgFallo = 'Conexión a la base de datos falló';
 
-    public __construct($this)
+    /**
+     * Constrctor
+     * $this es entrada
+     * @return this
+     */
+    function __construct()
     {
-        $this->connectDb()
+        $this->connectDb();
     }
 
     /**
      * Method for connect to DB
+     *
+     * @return this
      */
-    public function connectDb()
+    function connectDb()
     {
-        $this->con = mysqli_connect($this->dbhost, $this->dbuser, $this->dbpass, $this->dbname);
+        $this->_con = mysqli_connect($this->_dbhost, $this->_dbuser, $this->_dbpass, $this->_dbname);
         if (mysqli_connect_error()) {
-            die($_msgFallo . mysqli_connect_error() . mysqli_connect_errno());
+            die('Conexión a la base de datos falló: ' . mysqli_connect_error() . ' código error: ' . mysqli_connect_errno());
         }
+    }
+
+    /**
+     * Method clean the variable atfer save to db, for evitar inyeccion SQL.
+     *
+     * @var->inputvariable to function sanitize
+     * @return->this
+     */
+    function sanitize($var)
+    {
+        $ret = mysqli_real_escape_string($this->_con, $var);
+        return $ret;
+    }
+
+
+    /**
+     * Crear un registro en db mediante este funcion
+     *
+     * @param nombres            es entrada nombre de cliente
+     * @param apellidos          es entrada apellido de cliente
+     * @param telefono           es entrada telefono de cliente
+     * @param direccion          es entrada direccion de cliente
+     * @param correo_electronico es entrada correo electronico de cliente
+     *
+     * @return res resultado por correctamente un registro nuevo guardado en BD
+     */
+    function create($nombres,$apellidos,$telefono,$direccion,$correo_electronico)
+    {
+        $sql_insert = "INSERT INTO `clientes` (`nombres`, `apellidos`, `telefono`, `direccion`, `correo_electronico`)
+                        VALUES('$nombres',
+                                '$apellidos',
+                                '$telefono',
+                                '$direccion',
+                                '$correo_electronico')";
+
+        $insertadoClienteDb = mysqli_query($this->_con, $sql_insert);
+        return ($insertadoClienteDb) ? true : false;
+    }
+
+    function read()
+    {
+        $sql_query = "SELECT * FROM clientes";
+        return mysqli_query($this->_con, $sql_query);
+    }
+
+    function single_record($id)
+    {
+        $sql_query_id = "SELECT * FROM clientes WHERE id='$id'";
+        $ouput_sql = mysqli_query($this->_con, $sql_query_id);
+        return mysqli_fetch_object($ouput_sql);
+    }
+
+    function update($nombres,$apellidos,$telefono,$direccion,$correo_electronico,$id)
+    {
+        $sql_update = "UPDATE clientes SET  nombres             = '$nombres',
+                                            apellidos           = '$apellidos',
+                                            telefono            = '$telefono',
+                                            direccion           = '$direccion',
+                                            correo_electronico  = '$correo_electronico'
+                                        WHERE id=$id";
+        $actualizadoRegistrado = mysqli_query($this->_con, $sql_update);
+        return ($actualizadoRegistrado) ? true : false;
+    }
+
+    function delete($id)
+    {
+        $sql_delete = "DELETE FROM clientes WHERE id=$id";
+        $eliminadoDB = mysqli_query($this->_con, $sql_delete);
+        return ($eliminadoDB) ? true : false;
     }
 }
 ?>
--- src/update.php
+++ src/update.php
@@ -1,3 +1,10 @@
+<?php
+if (isset($_GET['id'])) {
+    $id = intval($_GET['id']);
+} else {
+    header('location:index.php');
+}
+?>
 <!DOCTYPE html>
 <html lang="es">
 
@@ -52,7 +59,7 @@
                     <!-- End div .col-sm-4 -->
                     <div class="col-sm-4 col-md-4">
                         <br>
-                        <h2 class="pmd-card-title-text" align="center">Agregar <strong>Cliente</strong></h2>
+                        <h2 class="pmd-card-title-text" align="center">Actualizar <strong>Cliente</strong></h2>
                     </div>
                     <!-- End div .col-sm-8 -->
                 </div>
@@ -87,27 +94,31 @@
                 $telefono           = $clientes->sanitize($_POST['telefono']);
                 $direccion          = $clientes->sanitize($_POST['direccion']);
                 $correo_electronico = $clientes->sanitize($_POST['correo_electronico']);
+                $id_cliente         = intval($_POST['id_cliente']);
 
-                $creadoNuevoRegistrodB = $clientes->create($nombres, $apellidos, $telefono, $direccion, $correo_electronico);
-                if ($creadoNuevoRegistrodB) {
-                    $stusT  = 'success';
+                $actualizadoClienteDb = $clientes->update($nombres, $apellidos, $telefono, $direccion, $correo_electronico, $id_cliente);
+
+                if ($actualizadoClienteDb) {
+                    $stusT = 'success';
                     $titleT = 'Bien hecho!';
-                    $msgT   = 'Los datos han sido guardados con éxito.';
-                    $class  = "alert alert-success";
-                    $msg    = 'Datos insertados con éxito';
+                    $msgT = 'Los datos han sido guardados con éxito.';
+                    $class = "alert alert-success";
+                    $msg = 'Datos actualizados con éxito';
                 } else {
-                    $stusT  = 'error';
+                    $stusT = 'error';
                     $titleT = 'Error';
-                    $msg    = $msgT = 'No se pudieron insertar los datos';
-                    $class  = 'alert alert-danger';
+                    $msgT = 'No se pudieron actualizar los datos';
+                    $class = 'alert alert-danger';
+                    $msg = 'No se pudieron actualizar los datos';
+                }
+                if (isset($msg) && isset($class)) {
+                    echo '<script>toastr.'.$stusT.'("'.$msgT.'", "'.$titleT.'", {timeOut: 6000, "closeButton": true, "progressBar": true})</script>';
+                    echo '<div class="'.$class.'">'.
+                    $msg.
+                    '</div>';
                 }
             }
-            if (isset($msg) && isset($class)) {
-                echo '<script>toastr.'.$stusT.'("'.$msgT.'", "'.$titleT.'", {timeOut: 6000, "closeButton": true, "progressBar": true})</script>';
-                echo '<div class="'.$class.'">'.
-                        $msg.
-                    '</div>';
-            }
+            $datos_cliente = $clientes->single_record($id);
             ?>
             <div class="container">
                 <div class="row">
@@ -119,28 +130,29 @@
                                         <label class="control-label">Nombres:</label>
                                         <input type="text" name="nombres" id="nombres" class="form-control" pattern="[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð]{3,48}"
                                             title="Por favor ingresa correctamente su primer nombre y si tiene segundo nombre"
-                                            required>
+                                            value="<?php echo $datos_cliente->nombres;?>" required>
+                                        <input type="hidden" name="id_cliente" id="id_cliente" class="form-control" maxlength="100" value="<?php echo $datos_cliente->id;?>">
                                         <span class="pmd-textfield-focused"></span>
                                     </div>
                                     <!-- End div nombres -->
                                     <div class="form-group col-sm-12 col-md-6 col-lg-6">
                                         <label class="control-label" for="">Apellidos:</label>
                                         <input type="text" name="apellidos" id="apellidos" class="form-control" pattern="[A-Za-zàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð]{3,48}"
-                                            title="Por favor ingresa correctamente su completo apellido" required>
+                                            title="Por favor ingresa correctamente su completo apellido" value="<?php echo $datos_cliente->apellidos;?>" required>
                                         <span class="pmd-textfield-focused"></span>
                                     </div>
                                     <!-- End div apellidos -->
                                     <div class="form-group col-sm-12 col-md-6 col-lg-6">
                                         <label for="" class="control-label">Teléfono:</label>
                                         <input type="tel" name="telefono" id="telefono" class="form-control" title="Por favor ingresa correctamente su número telefono"
-                                            pattern="{7,15}" required>
+                                            pattern="{7,15}" value="<?php echo $datos_cliente->telefono;?>" required>
                                         <span class="pmd-textfield-focused"></span>
                                     </div>
                                     <div class="form-group col-sm-12 col-md-6 col-lg-6">
                                         <label for="" class="control-label">Correo electronico:</label>
                                         <input type="email" name="correo_electronico" id="correo_electronico" class="form-control" title="Por favor ingresa correctamente su correo electrónico"
                                             pattern="^[a-zA-Z0-9.!#$%&amp;’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*${6,64}"
-                                            required>
+                                             value="<?php echo $datos_cliente->correo_electronico;?>" required>
                                         <span class="pmd-textfield-focused"></span>
                                     </div>
                                     <!-- End div correo_electronico -->
@@ -150,7 +162,7 @@
                                     <div class="form-group col-sm-12 col-md-12 col-lg-12 ">
                                         <label for="" class="control-label">Dirección:</label>
                                         <textarea type="text" name="direccion" id="direccion" class="form-control" pattern="[A-Za-zàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð]{3,255}"
-                                            title="Por favor ingresa correctamente su dirección" required></textarea>
+                                            title="Por favor ingresa correctamente su dirección" required><?php echo $datos_cliente->direccion;?></textarea>
                                         <span class="pmd-textfield-focused"></span>
                                     </div>
                                     <!-- End div direccion -->
@@ -171,9 +183,10 @@
                                     <div class="col-sm-1 col-md-1">&nbsp;</div>
                                     <div class="col-sm-5 col-md-3 col-lg-2">
                                         <div class="pmd-z-depth-2">
-                                        <a href="./index.php">
-                                            <button type="button" name="" id="cancelar" class="btn btn-block btn-danger">Cancelar</button>
-                                        </a>
+                                            <a href="./index.php">
+                                                <button type="button" name="" id="cancelar" class="btn btn-block btn-danger">Cancelar</button>
+                                            </a>
+                                            <!-- <input class="btn btn-success" type="submit" name="submit" id="submit" value="Guardar datos"> -->
                                             <!-- End div Cancelar -->
                                         </div>
                                         <!-- End div.depth -->
